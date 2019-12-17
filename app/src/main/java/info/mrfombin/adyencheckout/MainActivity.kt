1e259e8bc80b6929b5c7c01981978e619d2951c4
Willy Njundong <njundongw@gmail.com>
2019-12-17 14:31:47 -0500

    project setup

---
+++ app/src/main/java/info/mrfombin/adyencheckout/MainActivity.kt
@@ -0,0 +1,19 @@
+package info.mrfombin.adyencheckout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import info.mrfombin.adyencheckout.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/info/mrfombin/adyencheckout/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package info.mrfombin.adyencheckout.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
